b1a9a271a599f0cdcb3d42bcd13801cad6297b5a
Isha Mohan <ishamohano97@gmail.com>
2018-06-09 19:53:37 +0530

    redirect

---
+++ signupredirect.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Bank Redirect</title>
+	<link rel="stylesheet" type="text/css" href="StyleSheet/signupredirect.css">
+</head>
+<body>
+	<div class = "redirect-box">
+		<img src="Images/indexUserIcon.png" class = "redirect-user-icon">
+		<h1>Registered Successfully</h1>
+		<form>
+			<p>Your account has been created successfully.</p>
+			<input type="button" value="Click here to login" onclick="window.location.href='login.php'" />
+		</form>
+	</div>
+</body>
+</html>
---
+++ signupredirecterror.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Bank Redirect</title>
+	<link rel="stylesheet" type="text/css" href="StyleSheet/signupredirecterror.css">
+</head>
+<body>
+	<div class = "redirect-box">
+		<img src="Images/indexUserIcon.png" class = "redirect-user-icon">
+		<h1>Registration unsuccessful</h1>
+		<form>
+			<p>There was an error while creating your account.</p>
+			<input type="button" value="Click here to login" onclick="window.location.href='login.php'" />
+		</form>error
+	</div>
+</body>
+</html>
