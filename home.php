2364539940436a33da2b530fdbf891e5756fe7f0
Abhinav Sharma <abhinavsharma_an@srmuniv.edu.in>
2018-06-09 20:05:47 +0530

    home

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="StyleSheet/home.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="h1">
+<h1>Want to become a blood donor ?</h1>
+
+</div>
+	<div class="body">
+
+<div class="grid-container">
+<div>Who can give blood?
+          <br><br>
+                    <button type="button" class="button button1" onclick="location.href='check.php';">Check you can?</button>
+</div>
+
+<div>Become a blood donor
+          <br><br>
+                    <button type="button" class="button button2" onclick="location.href='signup.php';">Sign Up</button>
+</div>
+
+<div>&nbspAlready a donor?
+          <br><br>
+                    <button type="button" class="button button1" onclick="location.href='login.php';">Login</button>
+</div>
+
+<div>The donation process
+          <br><br>
+                    <button type="button" class="button button2" onclick="location.href='process.php';">Check process</button>
+</div>
+</div>
+</div>
+
+<div class="h2">
+<h1>#ITSTARTSWITHYOU</h1>
+</body>
+</html>
