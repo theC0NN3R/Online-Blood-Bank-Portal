fc57ed833df5f0a14e78dd51fde4b46aeb5f679a
Abhinav Sharma <abhinavsharma_an@srmuniv.edu.in>
2018-06-09 20:02:32 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="StyleSheet/check.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+	<img src="StyleSheet/12.jpg" alt="#">
+	<h1>DONORS GUIDELINES</h1>
+	
+	
+	
+	
+<h2>Creating an online account</h2>
+<p>It’s quick and easy. You'll need your name, email address and password
+create an account online. Later you can update your profile with contact number,
+blood group, address, date of birth and profile image.</p>
+
+
+
+<h2>Donation Process-Eat</h2>
+<p>Eating regularly before donating will help to keep your blood sugar levels stable. This is important so that you don’t feel lightheaded or dizzy after your donation. Having a snack before donation can help maintain these blood sugar levels. Ensuring that your diet contains foods rich in iron - such as meats and green leafy vegetables - will help to keep you feeling well during and after donation.</p>
+<h2>Donation Process-Drink</h2>
+
+
+<p>Almost half of the blood that you donate is made up of water. The fluids that you lose during donation can cause a drop in blood pressure – causing you to feel faint and dizzy.
+
+To help prevent this from happening we ask you to drink 500ml of water immediately before you give blood - we’ll give this to you before you donate.
+
+It’s also important to ensure that you are well hydrated in the days leading up to your donation. This will help to compensate for the fluids lost during donation, and will help to bring your blood volume levels back to normal.
+It is essential to avoid alcohol before and after donating as this may affect hydration levels and delay recovery.</p>
+
+
+<h2>Donation Process-Exercize</h2>
+<p>Avoid doing any vigorous exercise or heavy lifting the day of your donation – both before and after you’ve given blood.
+
+Keeping your body in a rested state is important to give it a chance to replenish the fluids lost during donation, which will help you avoid feeling dizzy or lightheaded and keep you well. Light exercise such as walking is fine, but please make sure that you are fully recovered and hydrated prior to your donation.</p>
+
+
+ 
+<h2>Donation Process-Water</h2>
+<p>We will need to roll up your sleeve past your elbow when you give blood – to allow easy access to your veins. To make sure that you are comfortable, please wear clothing with loose sleeves.</p>
+<h2>Donation Process-Distract</h2>
+<p>It is normal to feel nervous when you come along to donate – particularly if this is your first time. Our friendly and helpful staff will put you at ease straightaway. 
+Distraction is key. You may want to bring a friend to wait with you, or feel free to bring a book to read or listen to some music while you donate.
+
+We also ask donors to undertake something called ‘applied muscle tension’ or AMT. This is a simple behavioural technique that helps to maintain blood pressure, and so stops you from feeling faint or unwell. It is also a brilliant distraction technique! AMT involves tensing and relaxing the body’s major muscles during the donation process. A favourite of ours is clenching and unclenching your buttock muscles (no joke!).
+
+Further information about this will be available at your donation, or simply ask a member of staff who will be happy to advise you further.
+</p>
+
